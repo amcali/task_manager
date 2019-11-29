326310d614a4dd1754a2519f8d791e976912ff73
Ubuntu <ubuntu@ip-172-31-94-180.ec2.internal>
2019-11-29 02:48:28 +0000

    Updated.

---
+++ .c9/metadata/preview-https:/3e2fa5b2482448339a519e31b8fe1c62.vfs.cloud9.us-east-1.amazonaws.com
@@ -0,0 +1,1 @@
+{"changed":false,"filter":false,"title":"[P] https://3e2fa5b2482448339a519e31b8fe1c62.vfs.cloud9.us-east-1.amazonaws.com","value":"","undoManager":{"mark":-1,"position":-1,"stack":[]},"preview":{"path":"https://3e2fa5b2482448339a519e31b8fe1c62.vfs.cloud9.us-east-1.amazonaws.com","previewer":"previewBrowser","disableInjection":true}}
--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 import os
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)# "magic code" -- boilerplate
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb://root:r00tUs3r@myfirstcluster-shard-00-00-jr2lf.mongodb.net:27017,myfirstcluster-shard-00-01-jr2lf.mongodb.net:27017,myfirstcluster-shard-00-02-jr2lf.mongodb.net:27017/task_manager?ssl=true&replicaSet=myFirstCluster-shard-0&authSource=admin&retryWrites=true&w=majority'
+
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return "Hello world... again"
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 if __name__ == '__main__':
    app.run(host=os.environ.get('IP'),
